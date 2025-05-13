a75b56533f504f9485c8a9dff1cc47eb198a224f
Lazheart <176245760+Lazheart@users.noreply.github.com>
2025-05-13 17:52:24 -0500

    Final Implentation

--- src/main/java/com/example/demo/Events/Listener/Inventory/InventoryUpdateListener.java
+++ src/main/java/com/example/demo/Events/Listener/Inventory/InventoryUpdateListener.java
@@ -1,4 +1,18 @@
 package com.example.demo.Events.Listener.Inventory;
 
+import com.example.demo.Events.Listener.OrderCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class InventoryUpdateListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(InventoryUpdateListener.class);
+
+    @EventListener
+    public void manejarActualizacionInventario(OrderCreatedEvent event) {
+        logger.info("Reduciendo stock por el pedido ID: {}. Productos: {}", event.getOrder(), event.getProductos());
+    }
 }
